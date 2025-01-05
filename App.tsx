e2d575d40d5edb09d051c6b324df790b52f53d7d
J03LK <kevinperez44555@hotmail.com>
2025-01-04 21:46:45 -0500

    a medias

---
+++ App.tsx
@@ -0,0 +1,32 @@
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import GameScreen from './screens/GameScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LeaderboardScreen from './screens/LeaderboardScreen';
+
+// Tipos
+type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Game: undefined;
+  Leaderboard: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+// Componente principal
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
